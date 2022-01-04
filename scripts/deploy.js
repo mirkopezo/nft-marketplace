7026e2dfc3912f461294de854449f43bc90f70fd
mirkopezo <accfireover@tutanota.com>
2022-01-04 21:44:43 +0100

    Update deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+  const nftMarketplace = await NFTMarketplace.deploy();
+  await nftMarketplace.deployed();
+
+  console.log("Success! NFTMarketplace was deployed to: ", nftMarketplace.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
